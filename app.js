0459c308de3dbc50d698fcd704b59300171c9dd1
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-08 12:09:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+// var tableName=+prompt("ENter Table Name");
+
+
+// for(var i=1; i<=10; i++){
+//     console.log(tableName + "X" + i + "=" + tableName*i)
+// }
+
+
+
+
+
+
+
+
+// var str="*"
+
+// for (var i=0; i<15; i++){
+//     document.write("<h1>"+ str +"</h1>")
+//     str=str + "*"
+// }
+
+
+// var str="***********";
+
+
+// for(var i=0; i <11; i++){
+//     document.write("<h1>" + str + "</h1>")
+//     str=str.slice(0,str.length -1)
+// }
+
+
+// var str="********";
+// for(var i=0; i<7; i++){
+//     document.write("<h1>" +str+"</h1>")
+//     var str=str.slice(0,str.length-1);
+
+    
+// }
+
+
+
+
+
+// Nested loops
+
+
+
+// 5 round
+// 2 glass Water
+// 10pushups
+
+// for(var round=1; round<=5; round++){
+//     console.log("Round " + round + " Completed!")
+//     for(var water=1; water<=2; water++){
+//         console.log(water + " Drink Glass")
+//         for(var pushups=1; pushups<=10; pushups++){
+//             console.log("Completed " + pushups + " Pushups")
+//         }
+//     }
+// }
+
+
+
+
+
+// var city=["Karachi","lahore","Islamabad","Quetta","sakkar"]
+
+// var sli=city.slice(2,5)
+// console.log(sli)
+
+
+
+
+
+// var fruits=["Orange","Apple","Grapes","Banana","Date","Kiwi"]
+
+
+// var favfruits=fruits.slice(1,3);
+
+// console.log(favfruits)
+
+
+
+
+// var name1="Zeeshan Ahmed"
+
+// var sirName=name1.slice(8)
+// console.log(sirName)
+
+
+
+
+
+
+
+
+
+
+
+// var userName=prompt("What is Your Name?");
+
+// console.log(userName.toLowerCase());
+// console.log(userName.toUpperCase());
+
+
+
+
+// var userName=prompt("What is Your Name?");
+
+// var firstChar=userName[0];
+// var otherChar=userName.slice(1);
+
+// var userNameInTitleCase=firstChar.toUpperCase()+otherChar.toLowerCase();
+// console.log(userNameInTitleCase);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
